ccfa98b13503e43e36bc112ae3cb227e17e1c97d
JoeskiG <suminda.d.g@gmail.com>
2025-10-30 00:48:43 +1100

    constants file for payment gateway info

---
+++ src/lib/constants/payment.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_CHECKOUT_ACTION_URL =
+	'https://stagingxpay.info/index.php?route=checkout/billing'; // This is where the checkout form POSTs to (you can customise this in the configuration settings in the UI)

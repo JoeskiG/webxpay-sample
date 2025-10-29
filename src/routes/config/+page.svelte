4f0ff7e9bfc46f014ab5d23d01d44580b8ad5ef9
JoeskiG <suminda.d.g@gmail.com>
2025-10-30 00:54:25 +1100

    config editor page

---
+++ src/routes/config/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ConfigEditor from '$lib/components/config/config-editor.svelte';
+	import Container from '$lib/components/layout/container.svelte';
+</script>
+
+<Container>
+	<ConfigEditor />
+</Container>

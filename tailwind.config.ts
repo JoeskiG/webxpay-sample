f2b4a83a9ee4e5007acde01d1ff6363e9fb97ac3
JoeskiG <suminda.d.g@gmail.com>
2025-10-30 00:47:27 +1100

    first

---
+++ tailwind.config.ts
@@ -0,0 +1,59 @@
+import type { Config } from 'tailwindcss';
+
+//@ts-ignore
+import tailwindcssAnimate from 'tailwindcss-animate';
+
+const config: Config = {
+	darkMode: ['class'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	safelist: ['dark'],
+	theme: {
+		extend: {
+			colors: {
+				border: 'hsl(var(--border) / <alpha-value>)',
+				input: 'hsl(var(--input) / <alpha-value>)',
+				'main-background': 'hsl(var(--main-background) / <alpha-value>)',
+				background: 'hsl(var(--background) / <alpha-value>)',
+				foreground: 'hsl(var(--foreground) / <alpha-value>)',
+				success: 'hsl(var(--success) / <alpha-value>)',
+				'success-foreground': 'hsl(var(--success-foreground) / <alpha-value>)',
+				'destructive-foreground': 'hsl(var(--destructive-foreground) / <alpha-value>)',
+				'warn-foreground': 'hsl(var(--warn-foreground) / <alpha-value>)',
+				warn: {
+					DEFAULT: 'hsl(var(--warn) / <alpha-value>)',
+					foreground: 'hsl(var(--warn-foreground) / <alpha-value>)'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
+					foreground: 'hsl(var(--muted-foreground) / <alpha-value>)'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+					foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
+				},
+				info: {
+					DEFAULT: 'hsl(var(--info) / <alpha-value>)',
+					foreground: 'hsl(var(--info-foreground) / <alpha-value>)'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+					foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card) / <alpha-value>)',
+					foreground: 'hsl(var(--card-foreground) / <alpha-value>)'
+				}
+			},
+			borderRadius: {
+				xl: 'calc(var(--radius) + 4px)',
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
+				large: '2rem'
+			}
+		}
+	},
+	plugins: [tailwindcssAnimate]
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [sveltekit()],
+		define: {
+			'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV)
+		}
+	};
+});
